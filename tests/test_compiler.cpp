cb9aecdc6bc88437fca07c3477eb23e135fa3583
Rhys Fonville <rhys.fonville@gmail.com>
2023-11-10 22:22:07 -0500

    Added first test.

---
+++ tests/test_compiler.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <filesystem>
+#include "../compiler.cpp"
+
+namespace fs = std::filesystem;
+
+std::vector<std::string> get_files(const std::string &dir) {
+	std::vector<std::string> ret;
+    for (const auto &entry : fs::directory_iterator(dir))
+		ret.push_back(entry.path());
+
+	return ret;
+}
+
+int main(int argc, char* argv[]) {
+	std::vector<std::string> files = get_files("tests");
+	for (int i = 0; i < files.size(); i++) {
+		clog::out("+======================== TEST #" + std::to_string(i) + " ========================+");
+		
+		try {
+			begin_compile({ "rc", files[i] });
+		} catch (const std::exception &e) {
+			clog::error("Test #" + std::to_string(i) + " did not work. Message: " + e.what());
+		}
+
+		clog::out("+=========================================================+");
+	}
+}
+
